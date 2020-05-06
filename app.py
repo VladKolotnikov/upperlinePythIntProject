c70fc9046855ce77a85ad323e1988e6f4f3af31b
Siddharth Chalasani <siddharth.chalasani22@gmail.com>
2020-05-06 17:21:07 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+from model import get_location
 
 
 # -- Initialization section --
@@ -13,4 +14,20 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    props = {
+
+    }
+    return render_template("index.html",props=props)
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    if request.method == "POST":
+        activity = request.form["activity"]
+        location = get_location(activity)
+        props = {
+            "activity" : activity, 
+            "location" : location
+        }
+        return render_template("results.html", props=props)
+    else: 
+        return "error"
---
+++ model.py
@@ -0,0 +1,10 @@
+def get_location(activity):
+    location = {
+        "biking" : "Shark Valley Trail, Everglades National Park",
+        "skiing" : "Zermatt, Switzerland",
+        "hiking" : "Mount Kilimanjaro, Tanzania",
+        "surfing" : "Malibu, California",
+        "cliff-diving" : "Waimea Bay, Hawaii",
+        "rock-climbing" : "El Capitan"
+    }
+    return location[activity]
